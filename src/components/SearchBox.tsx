10b2adc1ae0d033421807a33bf4b5c7349dfbf77
taner <tanerozer16@gmail.com>
2023-12-21 19:32:38 +0300

    SearchBox add

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,46 @@
+import React, { useState, ChangeEvent, FormEvent, useRef } from "react";
+import "./SearchBox.css";
+
+interface SearchBoxProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBox: React.FC<SearchBoxProps> = ({ onSearch }) => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    onSearch(inputValue);
+  };
+
+  const handleButtonClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+      onSearch(inputRef.current.value);
+    }
+  };
+
+  return (
+    <div className="search-box-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={inputRef}
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Search the character..."
+        />
+        <button type="button" onClick={handleButtonClick}>
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBox;
