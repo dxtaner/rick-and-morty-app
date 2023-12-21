6441323c69d8770fdaf8cd08659fa0d96452b78a
taner <tanerozer16@gmail.com>
2023-12-21 19:30:31 +0300

    Header add

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";  
+
+const Header: React.FC = () => {
+  return (
+    <header className="App-header">
+      <h1>Rick and Morty Character Search</h1>
+      <p>Search for your favorite characters from Rick and Morty!</p>
+    </header>
+  );
+};
+
+export default Header;
