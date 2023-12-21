90304337a030e127d39135208d3ab635df7283fb
taner <tanerozer16@gmail.com>
2023-12-21 19:30:40 +0300

    LoadingMessage added

---
+++ src/components/LoadingMessage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LoadingMessage.css";
+
+const LoadingMessage: React.FC = () => {
+  return <p className="status-message loading">Loading...</p>;
+};
+
+export default LoadingMessage;
