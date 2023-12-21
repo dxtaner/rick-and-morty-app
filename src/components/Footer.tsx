38db5b859b3c2df0dc94fb803582e452e73d8d7e
taner <tanerozer16@gmail.com>
2023-12-21 19:30:10 +0300

    footer add

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <a
+        href="https://github.com/dxtaner"
+        target="_blank"
+        rel="noopener noreferrer">
+        Visit dxtaner on GitHub
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
