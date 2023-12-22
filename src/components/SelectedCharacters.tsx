cb772d1b956a1f10b420745bc083b97c0b89187b
taner <tanerozer16@gmail.com>
2023-12-22 10:37:53 +0300

    handleKeyDown added on SelectedCharacters.tsx

--- src/components/SelectedCharacters.tsx
+++ src/components/SelectedCharacters.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Character } from "../utils/Character";
 import "./SelectedCharacters.css";
 
@@ -11,14 +11,61 @@ const SelectedCharacters: React.FC<SelectedCharactersProps> = ({
   characters,
   onRemove,
 }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.focus();
+    }
+  }, []);
+
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLDivElement>,
+    character: Character
+  ) => {
+    if (
+      event.key === "ArrowUp" ||
+      event.key === "ArrowDown" ||
+      event.key === "ArrowLeft" ||
+      event.key === "ArrowRight"
+    ) {
+      event.preventDefault();
+      const index = characters.findIndex((c) => c.id === character.id);
+
+      if (index !== -1) {
+        if (
+          (event.key === "ArrowUp" || event.key === "ArrowLeft") &&
+          index > 0
+        ) {
+          (
+            containerRef.current?.children[index - 1] as HTMLDivElement
+          )?.focus();
+        } else if (
+          (event.key === "ArrowDown" || event.key === "ArrowRight") &&
+          index < characters.length - 1
+        ) {
+          (
+            containerRef.current?.children[index + 1] as HTMLDivElement
+          )?.focus();
+        }
+      }
+    } else if (event.key === "Enter") {
+      onRemove(character);
+    }
+  };
+
   if (characters.length === 0) {
     return null;
   }
 
   return (
-    <div className="selected-characters">
+    <div className="selected-characters" ref={containerRef} tabIndex={0}>
       {characters.map((character) => (
-        <div key={character.id} className="selected-character">
+        <div
+          key={character.id}
+          className="selected-character"
+          tabIndex={0}
+          onKeyDown={(e) => handleKeyDown(e, character)}>
           <span>{character.name}</span>
           <button onClick={() => onRemove(character)}>X</button>
         </div>
