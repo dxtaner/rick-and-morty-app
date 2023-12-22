a80a76d72167095bf9fdff99f0b188bea28b5616
taner <tanerozer16@gmail.com>
2023-12-22 08:33:11 +0300

    UnifiedSearchAndSelection add on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 
-import SearchBox from "./components/SearchBox";
 import ResultList from "./components/ResultList";
-import SelectedCharacters from "./components/SelectedCharacters";
 import Footer from "./components/Footer";
 import LoadingMessage from "./components/LoadingMessage";
 import ErrorMessage from "./components/ErrorMessage";
 import Header from "./components/Header";
 
 import { fetchCharactersByName } from "./api";
-import { Character } from "./components/Character";
+import { Character } from "./utils/Character";
+import UnifiedSearchAndSelection from "./components/UnifiedSearchAndSelection";
 
 const App: React.FC = () => {
   const [results, setResults] = useState<Character[]>([]);
@@ -50,19 +49,14 @@ const App: React.FC = () => {
     <div className="App" tabIndex={0}>
       <Header />
 
-      <SearchBox onSearch={handleSearch} />
+      <UnifiedSearchAndSelection
+        onSearch={handleSearch}
+        selectedCharacters={selectedCharacters}
+        onDeselect={handleDeselect}
+      />
 
       {loading && <LoadingMessage />}
       {error && <ErrorMessage message={error} />}
-
-      <SelectedCharacters
-        characters={selectedCharacters}
-        onRemove={(character) =>
-          setSelectedCharacters((prev) =>
-            prev.filter((char) => char.id !== character.id)
-          )
-        }
-      />
 
       <ResultList
         results={results}
